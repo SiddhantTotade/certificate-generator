42dfe9552b2ead88b1a9f2757f6953aae1f934b4
Siddhant Totade <siddhanttotade.1994@gmail.com>
2022-11-26 18:17:18 +0530

    created : sign in certificate

--- certificate_generator.py
+++ certificate_generator.py
@@ -3,10 +3,10 @@ import pandas as pd
 import os
 import glob
 
+
 def cleanup_data():
     files = glob.glob('generated-certificates/*')
     for f in files:
-        print(f)
         os.remove(f)
 
 
@@ -19,10 +19,23 @@ def generate_certificates():
     to_dates = df['Event To Date'].tolist()
     for date in from_dates:
         from_date.append(str(date.date()))
-    print(type(from_date))
 
     for index, names in enumerate(key):
-        template = cv2.imread("certificate-generator/cretificate-template.jpg")
+        template = cv2.imread("certificate-generator/certificate-template.jpg")
+        signature = cv2.imread("certificate-generator/Galvin Belson.png", -1)
+
+        x_offset = y_offset = 50
+
+        y1, y2 = y_offset, y_offset + signature.shape[0]
+        x1, x2 = x_offset, x_offset + signature.shape[1]
+
+        alpha_s = signature[:, :, 3] / 255.0
+        alpha_l = 1.0 - alpha_s
+
+        for c in range(0, 3):
+            template[y1:y2, x1:x2, c] = (
+                alpha_s * signature[:, :, c] + alpha_l * template[y1:y2, x1:x2, c])
+
         cv2.putText(template, names, (500, 405),
                     cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 2, (0, 0, 255), 1, cv2.LINE_AA)
         cv2.putText(template, from_date[index], (782, 552),
@@ -32,4 +45,4 @@ def generate_certificates():
 
 
 cleanup_data()
-generate_certificates()
+# generate_certificates()
