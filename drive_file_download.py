80a18b6d38a96a2e1b161a71f04defcc808b7171
Siddhant Totade <siddhanttotade.1994@gmail.com>
2023-03-21 00:38:37 +0530

    modified : drive_file_download deleted

--- drive_file_download.py
+++
@@ -1,9 +0,0 @@
-from zdrive import Downloader
-
-output_dir = "drive-files/"
-
-d = Downloader()
-
-folder_id = '1kj4rSLzqMAYScAQgiIg0g_wqtlkIFyNA'
-
-d.downloadFolder(folder_id, destinationFolder=output_dir)
